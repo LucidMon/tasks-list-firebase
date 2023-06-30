5cd03223e7a38f39564344ca1afc3564daa34321
LucidMon <ldonm.code@gmail.com>
2023-06-30 12:35:32 -0400

    minor fix LogoApp

--- src/components/LogoApp.tsx
+++ src/components/LogoApp.tsx
@@ -5,7 +5,7 @@ import ImageLogo  from '../assets/images/portapapeles-icon.jpg'
 export const LogoApp = ({size}: {size: number}) => {
     return(
         <div className="LogoApp" style={{display:'flex', alignItems:'center', fontSize:`${size*0.3571}px`, fontStyle:'italic'}}>
-            <span style={{textDecoration:'line-through', textDecorationThickness:'4px', textDecorationColor:'#EA4235'}}>To</span>
+            <span style={{textDecoration:'line-through', textDecorationThickness:`${(size*0.3571)*0.08}px`, textDecorationColor:'#EA4235'}}>To</span>
             <img style={{width:`${size*0.7142}x`, height:`${size}px`, marginRight:'10px', marginLeft:'10px'}} src={ImageLogo} alt="ImageLogo" />
             <span>Do</span>
         </div>
