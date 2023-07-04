22dd2bf1f1a06ddb944d832383292c581b3dbccf
LucidMon <ldonm.code@gmail.com>
2023-07-04 13:27:30 -0400

    add Lobby page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { Home } from "./pages/Home"
 import { Register } from "./pages/Register"
+import { Lobby } from "./pages/Looby"
 
 export const Router = () => {
     return(
@@ -11,6 +12,7 @@ export const Router = () => {
                     <Route path="/" element={<Home/>}></Route>
                     <Route path="/home" element={<Home/>}></Route>
                     <Route path="/register" element={<Register/>}></Route>
+                    <Route path="/lobby" element={<Lobby/>}></Route>
                 </Routes>
             </BrowserRouter>
         </>
